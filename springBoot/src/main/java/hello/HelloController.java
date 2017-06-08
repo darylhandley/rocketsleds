3971ffc9886589bb7a4b1083cb8aef4742239e55
darylhandley <dhandley@xmatters.com>
2017-06-08 09:55:18 -0700

    none

--- springBoot/src/main/java/hello/HelloController.java
+++ springBoot/src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import model.Person;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,11 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/person")
+    public Person getPerson() {
+        return new Person().setFirstName("Daryl").setLastName("Handley");
     }
     
 }
