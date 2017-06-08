430715b5ab6ae220b38fa4e40473c58bce6e43a8
darylhandley <dhandley@xmatters.com>
2017-06-08 11:00:23 -0700

    none

--- springBoot/src/main/java/hello/MainClass.java
+++ springBoot/src/main/java/hello/MainClass.java
@@ -1,8 +1,11 @@
 package hello;
 
+import com.google.common.collect.ImmutableMap;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
 public class MainClass {
 
     public static void main(String[] args) {
-
+        ImmutableMap.of("", "");
     }
 }
